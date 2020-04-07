8e1064e8e4bf0e4d7f94eda55684f1790b962f08
Andre <alosbhgmail.com>
2020-04-07 12:17:17 -0300

    autenticação login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {Feather} from '@expo/vector-icons';
 import {useNavigation} from '@react-navigation/native'
-import {View,Image, Text, TouchableOpacity, TextInput} from 'react-native';
+import {View,Image, Text, TouchableOpacity, TextInput,Alert} from 'react-native';
 import api from '../../services/api';
 import logoImg from '../../assets/logo.png';
 
@@ -9,14 +9,45 @@ import styles from './styles'
 export default function Login(){
 
     const navigation = useNavigation();
+    
+    const [Email, setEmail] = useState('');
+
+    const [Password, setPassword] = useState('');
 
     function navigatetoRegister(){
 
         navigation.navigate('Register');
     }
 
-    function Submit(){
-        navigation.navigate('Home');
+    async function Submit(){
+        
+        data= {Email, Password}
+
+        try {
+            const response = await api.post('Labor/login',data);
+            
+            if(response.status===200){
+                Alert.alert(
+                    '',
+                    'Login realizado com sucesso',
+                    [
+                      {text: 'OK', onPress: () => {navigation.navigate('Home')}},
+                    ],
+                    { cancelable: false }
+                  )
+
+                
+            }
+            else if(response.status===202){
+                Alert.alert("Senha incorreta. Tente novamente")
+            }
+            
+        }
+        catch{
+            Alert.alert('Erro no login. Tente novamente')
+        }
+
+        
     }
 
     return(
@@ -26,8 +57,22 @@ export default function Login(){
         </View>
         <View style={styles.form}>
             <Text style={styles.title}>Entre com seu e-mail:</Text>
-            <TextInput style={styles.input} placeholder={"Email"}/>
-            <TextInput style={styles.input} placeholder={"Senha"}/>
+            
+            <TextInput
+                style={styles.input} 
+                placeholder={"Email"}
+                value={Email}
+                onChangeText={text=> setEmail(text)}   
+            />
+            <TextInput
+                style={styles.input} 
+                placeholder={"Senha"}
+                secureTextEntry={true}
+                value={Password}
+                onChangeText={text=> setPassword(text)}   
+            />
+
+            
             <TouchableOpacity onPress={navigatetoRegister}>
                 <Text>Não tenho cadastro</Text>
             </TouchableOpacity>
